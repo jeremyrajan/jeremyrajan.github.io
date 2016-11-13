d0bed53dae3e2a78aa72e474b83e08b323f4f2eb
Jeremy Rajan <jeremyrajan@gmail.com>
2016-11-14 00:15:51 +0800

    localStorage support

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -8,15 +8,35 @@ const Project = (props) => (
   <li><a className="button" target="_blank" href={props.link}>{props.name}</a></li>
 );
 
+const fromLocalStorage = () => {
+  if (!window.localStorage) {
+    return;
+  }
+
+  return JSON.parse(localStorage.getItem('jeremy_rajan_github'));
+};
+
+const toLocalStorage = (key, val) => {
+  if (!window.localStorage) {
+    return;
+  }
+
+  localStorage.setItem(key, JSON.stringify(val));
+  return true; 
+}
+
 class Projects extends React.Component {
   constructor() {
     super();
     this.state = {
-      projects: []
+      projects: fromLocalStorage() || []
     }
-    jeremyrajan.listRepos()
-      .then(({data: reposJson}) => {
-        this.setState({projects: reposJson.filter(f => !f.fork 
+
+    // if we cant find in localStorage then continue
+    if (!this.state.projects.length) {
+      jeremyrajan.listRepos()
+      .then(({ data: reposJson }) => {
+        const repos = reposJson.filter(f => !f.fork 
             && !f.name.includes('github.io') 
             && !f.name.includes('photoSticker')
             && !f.name.includes('MySite')
@@ -24,9 +44,11 @@ class Projects extends React.Component {
             && !f.name.includes('angular-seed')
             && !f.name.includes('Codeignitor')
             && !f.name.includes('Wordpress-Blank')
-            && !f.name.includes('React-Fun'))
-        });
+            && !f.name.includes('React-Fun'));
+        toLocalStorage('jeremy_rajan_github', repos); // store to localStorage
+        this.setState({projects: repos });
       });
+    }
   }
 
   render() {
@@ -38,7 +60,7 @@ class Projects extends React.Component {
       );
     }
 
-    const projects = this.state.projects.map((p, key) =>  <Project key={key} name={p.name} link={p.html_url} />)
+    const projects = this.state.projects.map((p, key) =>  <Project key={key} name={p.name} link={p.html_url} />);
     return (
       <div id="projects">
         <h1>OSS</h1>
